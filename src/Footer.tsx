ca0be4d8649b73567c4ca513c43ba2affb7db5ea
szpanel <szpanelek@gmail.com>
2021-04-09 19:39:27 +0200

    :lipstick: Footer redesign

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,60 +1,45 @@
 import styled from "styled-components";
-import {Box, Grid, Theme, Typography, useTheme} from "@material-ui/core";
+import {Box, Theme, Typography, useTheme} from "@material-ui/core";
 import styles from "./styles/_main.module.scss";
 import {Email, Facebook, GitHub} from "@material-ui/icons";
+import {createStyles, makeStyles} from "@material-ui/styles";
 
 const FooterContainer = styled.footer<{ theme: Theme }>`
   z-index: 100;
   width: 100%;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   padding: 16px;
   background-color: ${props => props.theme.palette.secondary.main};
   height: ${styles.footerHeight};
 `;
 
-const Link = styled.a`
-  color: white;
-  text-decoration: none;
-  padding-left: 8px;
-  font-weight: bold;
-`;
+const useStyles = makeStyles(() =>
+    createStyles({
+        cursorPointer: {
+            cursor: "pointer"
+        }
+    }),
+);
 
 const Footer = () => {
     const theme = useTheme();
+    const classes = useStyles();
 
     return <FooterContainer theme={theme}>
-        <Grid container justify="space-between" alignItems="center">
-            <Grid item xs={4}>
-                <Grid container direction="column" spacing={2}>
-                    <Grid item xs={12}>
-                        <Box display="flex" alignItems="center">
-                            <GitHub/><Link theme={theme} href="https://github.com/szpanel">@szpanel</Link>
-                        </Box>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Box display="flex" alignItems="center">
-                            <Facebook/><Link theme={theme} href="https://facebook.com/lukasz.szpanelewski">Łukasz
-                            Szpanelewski</Link>
-                        </Box>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Box display="flex" alignItems="center">
-                            <Email/><Link theme={theme}
-                                          href="mailto:szpanelek@gmail.com">szpanelek@gmail.com</Link>
-                        </Box>
-                    </Grid>
-                </Grid>
-            </Grid>
-            <Grid item xs={4}>
-                <Grid container justify="center" alignItems="center">
-                    <Grid item><Typography color="primary">© 2021 Lukasz Szpanelewski. All rights
-                        reserved.</Typography></Grid>
-                </Grid>
-            </Grid>
-            <Grid item xs={4}/>
-        </Grid>
+        <Box display="flex" gridGap={16}>
+            <GitHub className={classes.cursorPointer}
+                    onClick={() => window.location.href = "https://github.com/szpanel"}/>
+            <Facebook className={classes.cursorPointer}
+                      onClick={() => window.location.href = "https://facebook.com/lukasz.szpanelewski"}/>
+            <Email className={classes.cursorPointer}
+                   onClick={() => window.location.href = "https://facebook.com/lukasz.szpanelewski"}/>
+        </Box>
+        <Box>
+            <Typography color="primary" align="center" variant="h6">© 2021 Lukasz Szpanelewski. All rights
+                reserved.</Typography>
+        </Box>
     </FooterContainer>
 }
 
