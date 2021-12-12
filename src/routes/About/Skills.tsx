5565e01986cebbed1cd26e4910f4789b7404ddc8
szpanel <szpanelek@gmail.com>
2021-12-13 00:52:12 +0100

    refactor(skills): Add new & modify old skills

--- src/routes/About/Skills.tsx
+++ src/routes/About/Skills.tsx
@@ -18,21 +18,18 @@ const BorderLinearProgress = withStyles((theme: Theme) =>
 enum Skill {
     FIREBASE = "Firebase",
     JAVA = "Java",
-    SPRING = "Spring",
     GIT = "Git",
     KOTLIN = "Kotlin",
     JAVASCRIPT = "JavaScript",
     ANDROID = "Android",
     REACT = "React",
+    REACT_NATIVE = "React Native",
     NODE = "Node.JS",
-    HTMLCSS = "HTML/CSS/JS",
-    LINUX = "Linux",
-    CPP = "C++",
-    PHP = "Php",
     JIRA = "Jira",
     AWS = "AWS S3",
     MONGO = "Mongo",
     MYSQL = "MySQL",
+    GRAPHQL = "GraphQL",
 }
 
 interface SkillMeta {
@@ -49,15 +46,11 @@ const Skills = () => {
         },
         [Skill.JAVA]: {
             current: 0,
-            max: 80
-        },
-        [Skill.SPRING]: {
-            current: 0,
-            max: 30
+            max: 65
         },
         [Skill.GIT]: {
             current: 0,
-            max: 50
+            max: 65
         },
         [Skill.KOTLIN]: {
             current: 0,
@@ -65,39 +58,27 @@ const Skills = () => {
         },
         [Skill.JAVASCRIPT]: {
             current: 0,
-            max: 75
+            max: 95
         },
         [Skill.ANDROID]: {
             current: 0,
-            max: 35
+            max: 40
         },
         [Skill.REACT]: {
             current: 0,
-            max: 45
+            max: 70
+        },
+        [Skill.REACT_NATIVE]: {
+            current: 0,
+            max: 65
         },
         [Skill.NODE]: {
             current: 0,
-            max: 35
-        },
-        [Skill.HTMLCSS]: {
-            current: 0,
-            max: 60
-        },
-        [Skill.LINUX]: {
-            current: 0,
-            max: 45
-        },
-        [Skill.CPP]: {
-            current: 0,
-            max: 10
-        },
-        [Skill.PHP]: {
-            current: 0,
-            max: 15
+            max: 55
         },
         [Skill.JIRA]: {
             current: 0,
-            max: 15
+            max: 50
         },
         [Skill.AWS]: {
             current: 0,
@@ -109,7 +90,11 @@ const Skills = () => {
         },
         [Skill.MYSQL]: {
             current: 0,
-            max: 35
+            max: 45
+        },
+        [Skill.GRAPHQL]: {
+            current: 0,
+            max: 40
         },
     });
 
